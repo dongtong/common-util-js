589b1de72055ccc57635834389ed281c3ee34dc7
djh <jinhu.tung@gmail.com>
2015-11-06 21:34:56 +0800

    验证手机号

---
+++ util.js
@@ -0,0 +1,19 @@
+/*
+* @class util
+* 常用工具方法
+/
+
+var Util = (function(){
+    
+    /*
+    * @function isValidMobile
+    * @param 
+    /
+    function isValidMobile(mobile) {
+        var mobileRegx = /(^(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7})$/;
+        if(!mobileRegx.test(mobile)) {
+          alert('电话号码非法')
+        }
+    }
+    
+})();
